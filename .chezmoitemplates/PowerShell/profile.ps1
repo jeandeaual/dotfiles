4f1f5360eb108d96d50b2bf6a78a83f4043165f0
Alexis Jeandeau <alexis.jeandeau@gmail.com>
2021-09-12 23:39:24 +0900

    Add a few shell aliases

--- .chezmoitemplates/PowerShell/profile.ps1
+++ .chezmoitemplates/PowerShell/profile.ps1
@@ -1,3 +1,32 @@
-function chezmoicd {
-    Set-Location $(chezmoi source-path)
+# Aliases
+#------------------------------------------------------------------------------
+
+if (Get-Command go -ErrorAction SilentlyContinue) {
+    function gor {
+        go build -ldflags='-s -w' $args
+    }
+}
+
+if (Get-Command flac -ErrorAction SilentlyContinue) {
+    function flacreencode {
+        Get-ChildItem -Path . -Filter *.flac -Recurse | ForEach-Object {
+            flac --verify --best --decode-through-errors --preserve-modtime -e -p -f $_
+        }
+    }
+}
+
+# Custom functions
+#------------------------------------------------------------------------------
+
+function mkcd {
+    if (!(Test-Path -Path $args[0])) {
+        mkdir $args[0] > $null
+    }
+    Set-Location $args[0] -PassThru
+}
+
+if (Get-Command chezmoi -ErrorAction SilentlyContinue) {
+    function chezmoicd {
+        Set-Location $(chezmoi source-path)
+    }
 }
